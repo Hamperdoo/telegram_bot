0771722fd54c09b83511b3ab8d89d4803547d555
Humperdoo <Bumbox90-90@yandex.ru>
2023-07-24 15:43:49 +0300

    second commit (command, text availablle, photo, echo)

--- mybot.py
+++ mybot.py
@@ -4,7 +4,23 @@ from config import TOKEN
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+# Бот реагирует на определенные команды в чате
+@dp.message_handler(commands=['start', 'welcome', 'about'])
+async def cmd_handler(message: types.Message):
+    await message.answer('its help bot')
 
+# Бот реагирует на наличие определенной фразы в чате (в том числе в исправленном сообщении)
+@dp.message_handler(lambda message: message.text and 'hello' in message.text.lower())
+@dp.edited_message_handler(lambda message: message.text and 'hello' in message.text.lower())
+async def msg_handler(message: types.Message):
+    await message.answer('И тебе привет')
+
+# Бот реагирует на изображения в чате
+@dp.message_handler(content_types=types.ContentType.PHOTO)
+async def audio_handler(message: types.Message):
+    await message.answer('Крутая фотка')
+
+# Бот возвращает написанное обратно в чат
 @dp.message_handler()
 async def echo(message: types.Message):
 
